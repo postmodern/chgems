4f77b195f0cd2859c5c1446bff0d3114d5f9e3f2
Postmodern <postmodern.mod3@gmail.com>
2012-12-06 19:35:50 -0800

    Homepage goes first.

--- homebrew/chgems.rb
+++ homebrew/chgems.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Chgems < Formula
 
-  url 'https://github.com/downloads/postmodern/chgems/chgems-0.2.4.tar.gz'
   homepage 'https://github.com/postmodern/chgems#readme'
+  url 'https://github.com/downloads/postmodern/chgems/chgems-0.2.4.tar.gz'
   sha1 '12346202c21f0b0ecb91cb9f74abd10ab3fc5384'
   head 'https://github.com/postmodern/chgems.git'
 
