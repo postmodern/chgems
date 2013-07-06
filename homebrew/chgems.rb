c94b5df9698675f7f227fa6bd83aabaf13644bc7
Postmodern <postmodern.mod3@gmail.com>
2013-07-05 17:30:57 -0700

    Updated the homebrew formula for 0.3.2.

--- homebrew/chgems.rb
+++ homebrew/chgems.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Chgems < Formula
 
   homepage 'https://github.com/postmodern/chgems#readme'
-  url 'https://github.com/postmodern/chgems/archive/v0.3.1.tar.gz'
-  sha1 '742f1d12e85d93f286472425618fceba3e01f67c'
+  url 'https://github.com/postmodern/chgems/archive/v0.3.2.tar.gz'
+  sha1 '73775a7d57e61ca895ae11cebc028f1ab8150977'
   head 'https://github.com/postmodern/chgems.git'
 
   def install
